06acd4afeacb30d22cbad7648a62f9b20275cacc
Chrisfufu <zuofu@ualberta.ca>
2020-04-22 10:54:59 -0600

    validator finished

--- myApp/api/serializers.py
+++ myApp/api/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 from rest_framework.serializers import (
     ModelSerializer,
 )
-
+from django.core.validators import validate_email
 
 class ContactSerializer(ModelSerializer):
     class Meta:
@@ -17,3 +17,11 @@ class ContactSerializer(ModelSerializer):
             'phone',
             'photo'
         ]
+
+    # def validate_email(self, value):
+    #     data = self.get_initial()
+    #     email = value
+    #     print(email)
+    #     # needs to fix. if is_active == 0, need to delete the existing user and raise no error
+    #     validate_email(email)
+        # return value
--- myApp/models.py
+++ myApp/models.py
@@ -1,15 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.crypto import get_random_string
-import uuid
+from django.core.validators import RegexValidator
+from django.core.validators import validate_email
 # Create your models here.
 
 class Contact(models.Model):
     id = models.AutoField(primary_key=True)
     firstName = models.TextField(null=True)
     lastName = models.TextField(null=True)
-    email = models.TextField(null=True)
-    phone = models.IntegerField(null=True)
+    email = models.EmailField(
+                null=True, validators=[validate_email],
+                error_messages={'required': 'Please provide your email address.',
+                        'unique': 'An account with this email exist.'}
+                )
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone = models.CharField(validators=[phone_regex], max_length=17, blank=True)
     photo = models.ImageField(upload_to='photos',null=True)
     class Meta:
         verbose_name = "Contact"
