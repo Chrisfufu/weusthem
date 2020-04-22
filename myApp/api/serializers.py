bbfb7d2c573df1a5fc7c9b85e0f62ee934e34b52
Chrisfufu <zuofu@ualberta.ca>
2020-04-22 10:22:37 -0600

    delete unused library

--- myApp/api/serializers.py
+++ myApp/api/serializers.py
@@ -1,24 +1,8 @@
 from rest_framework import serializers
 from myApp.models import *
-from django.db.models import Q
-from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-from rest_framework_jwt.settings import api_settings
-from django.db import transaction
-import uuid
-from django.utils import timezone
-from django.core.mail import EmailMessage
-from django.conf import settings
-from django.contrib.auth.validators import UnicodeUsernameValidator
 from rest_framework.serializers import (
     ModelSerializer,
-    SerializerMethodField,
-    CharField,
-    EmailField,
-    ValidationError,
-    HyperlinkedIdentityField,
-    ListSerializer,
-    Serializer
 )
 
 
--- myApp/api/views.py
+++ myApp/api/views.py
@@ -1,21 +1,6 @@
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from rest_framework import status
-from rest_framework.views import APIView
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from myApp.models import *
 from .serializers import *
-from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
-from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
-from rest_framework.generics import GenericAPIView
-from rest_framework.mixins import UpdateModelMixin
-from rest_framework.permissions import (
-    AllowAny,
-    IsAuthenticated,
-    IsAdminUser,
-    IsAuthenticatedOrReadOnly,
-)
+
 from rest_framework.generics import (
     ListAPIView,
     RetrieveAPIView,
@@ -24,7 +9,7 @@ from rest_framework.generics import (
     CreateAPIView,
     DestroyAPIView,
 )
-#
+
 class ContactCreateAPIView(CreateAPIView):
     serializer_class = ContactSerializer
     queryset = Contact.objects.all()
